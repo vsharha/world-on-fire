0c9f16b1efdc5649f506df32b7e6c658693e1760
Viktor Sharha <vsharha07@gmail.com>
2025-11-01 23:44:02 +0000

    add tanstack query

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -2,9 +2,17 @@
 
 import * as React from "react"
 import ThemeProvider from "@/components/ThemeProvider";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {QueryClientProvider} from "@tanstack/react-query";
+import {queryClient} from "@/lib/react-query-client";
 
 export function Providers({ children }) {
-  return <ThemeProvider>{children}</ThemeProvider>
+  return <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false}/>
+      </QueryClientProvider>
+  </ThemeProvider>
 }
 
 export default Providers;
