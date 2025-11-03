343948307127c44cec5242a32a39ad97a7fb6f5a
Viktor Sharha <vsharha07@gmail.com>
2025-11-03 17:25:40 +0000

    fix homepage

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
       <main className="flex-1 flex flex-col">
           <div className="flex-1">
-              <section className="relative flex flex-col items-center justify-center w-full gap-5 min-h-120 bg-none overflow-hidden rounded-lg">
+              <section className="relative flex flex-col items-center justify-center w-full gap-5 h-90 bg-none overflow-hidden rounded-lg p-5">
                   <div className="absolute inset-0 bg-[url('/bg.png')] bg-cover bg-center filter blur-xl -z-10" />
                   <h1 className="text-3xl font-bold text-center">See what&apos;s going on in the world, right now.</h1>
                   <h2 className="text-xl">Get a new perspective on life.</h2>
@@ -15,7 +15,7 @@ export default function Home() {
                   </Link>
               </section>
               <div className="w-full flex items-center justify-center">
-                <NewsFeed className="w-full max-w-150 mt-3" defaultOpen={true}/>
+                <NewsFeed className="w-full max-w-150 m-2 mt-3" defaultOpen={true}/>
               </div>
           </div>
           <footer className="py-4 text-center text-sm text-muted-foreground">
